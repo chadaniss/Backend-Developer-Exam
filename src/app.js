61231a372c4973d2aaae3985a458a1a21aaabb7a
chadaniss <job.chadanis@gmail.com>
2022-11-22 01:42:10 +0700

    setting cors

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ const todoRoute = require('./routes/todoRoute');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 const notFoundMiddleware = require('./middlewares/notFoundMiddleware');
 const port = 8000;
+const cors = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/todos', todoRoute);
 app.use(notFoundMiddleware);
